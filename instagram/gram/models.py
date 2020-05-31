1f3d74cf169f2dd057a7db1521c77dbf51b73189
Kvria <trishakuria@gmail.com>
2020-05-31 12:04:55 +0300

    add category model

--- instagram/gram/models.py
+++ instagram/gram/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here
+class Category(models.Model):
+    category_name = models.CharField(max_length = 45)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.category_name
+
+    @classmethod
+    def update_category(id,value):
+        Category.objects.filter(id = id).update(name = value)\
+    
+    @classmethod
+    def display_all_categories(cls):
+        return cls.objects.all()
+
