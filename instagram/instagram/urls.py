8f0b4ff0d327fecac81088fd1ca54810a592953c
Kvria <trishakuria@gmail.com>
2020-06-01 12:17:41 +0300

    removed models

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import url, include
+
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^accounts/', include('registration.backends.simple.urls'),
     url(r'^admin/', admin.site.urls),
-    url(r'',include('gram.urls'))
-    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^$',include('gram.urls')),
+    # url(r'^accounts/', include('registration.backends.simple.urls'),
+    # url(r'^logout/$', views.logout, {"next_page": ''})
 
 ]
+   
