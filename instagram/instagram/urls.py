49fe33b0498eaef13811cb85425f5a6a74e601b7
Kvria <trishakuria@gmail.com>
2020-06-01 09:15:31 +0300

    add registration form

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path("register/", v.register, none="register"),
-    path('', include("django.contrib.auth.urls"))
+    url(r'^admin/', admin.site.urls),
+    url(r'^register/', v.register, none="register"),
+    url(r'^/', include("django.contrib.auth.urls"))
 ]
