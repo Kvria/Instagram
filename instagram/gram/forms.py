a0a28620c5a4a7afe4a79678d158e55f1429efb4
Kvria <trishakuria@gmail.com>
2020-06-03 07:18:07 +0300

    add profile update form

--- instagram/gram/forms.py
+++ instagram/gram/forms.py
@@ -15,5 +15,11 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image',]
         
         
